ae05f130ab288634ad8f95fdd18288851660e549
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-07-20 19:45:06 +0200

    Add lombok getter/setter generation for the User class

--- src/main/java/com/sample/domain/User.java
+++ src/main/java/com/sample/domain/User.java
@@ -1,9 +1,14 @@
 package com.sample.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
@@ -23,36 +28,4 @@ public class User implements Serializable {
     )
     @Column(name = "authority")
     private List<String> authorities;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public List<String> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(List<String> authorities) {
-        this.authorities = authorities;
-    }
 }
