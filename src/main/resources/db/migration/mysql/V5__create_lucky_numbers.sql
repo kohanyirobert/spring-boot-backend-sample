c5b3b1cc078d787f9063a893bab4f291972db273
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-07-24 10:28:15 +0200

    Update lucky_numbers flyway scripts
    MySQL ignores check constraints, created triggers to simulate it.

--- src/main/resources/db/migration/mysql/V5__create_lucky_numbers.sql
+++ src/main/resources/db/migration/mysql/V5__create_lucky_numbers.sql
@@ -1,7 +1,36 @@
 create table lucky_numbers (
     id int auto_increment primary key,
     user_id int not null,
-    number int not null check (number >= 0 && number < 10),
+    number int not null check (number >= 0 && number <= 9),
     foreign key (user_id) references users (id),
     unique (user_id, number)
-)
+);
+
+-- MySQL ignores check constraints, but we can simulate it with triggers.
+-- The 45000 SQLSTATE class is used to signal errors to the calling code.
+delimiter $$
+
+create procedure check_parts (in number int)
+begin
+    if number < 0 or number > 9
+    then
+        signal sqlstate '45000' set message_text = 'check constraint on lucky_numbers.number failed';
+    end if;
+end
+$$
+
+create trigger check_number_before_insert before insert on lucky_numbers
+for each row
+begin
+    call check_parts(new.number);
+end
+$$
+
+create trigger check_number_before_update before update on lucky_numbers
+for each row
+begin
+    call check_number(new.number);
+end
+$$
+
+delimiter ;
--- src/main/resources/development/db/migration/mysql/V2__add_admin_user.sql
+++ src/main/resources/development/db/migration/mysql/V2__add_admin_user.sql
@@ -1,2 +1,4 @@
+-- id: 1, username: admin, password: admin
 insert into users (username, password, enabled) values ('admin', '$2a$10$2Gi3G9XaKalERoIa74OYruEHZyqSUqn10uSiOzk4PvOgL49vejna.', true);
+
 insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
