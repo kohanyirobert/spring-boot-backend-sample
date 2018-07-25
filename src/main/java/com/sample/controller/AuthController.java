6959cfc8c7ce896f4533197c2dbb75c62330c7e0
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-07-25 09:33:04 +0200

    Return User instead of Principal on successful auth at /auth

--- src/main/java/com/sample/controller/AuthController.java
+++ src/main/java/com/sample/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.sample.controller;
 
+import com.sample.domain.User;
+import com.sample.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,13 @@ import java.security.Principal;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("")
-    public Principal get(Principal principal) {
-        return principal;
+    public User get(Principal principal) {
+        return userRepository.findByUsername(principal.getName())
+            .orElseThrow();
     }
 
     @DeleteMapping("")
