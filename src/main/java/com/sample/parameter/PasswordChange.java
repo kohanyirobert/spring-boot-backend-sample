5f96a46545edccacb5460ce01f4e99458ebad27c
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-07-25 16:16:41 +0200

    Update PasswordChange: make fields final so that lombok generates a ctor

--- src/main/java/com/sample/parameter/PasswordChange.java
+++ src/main/java/com/sample/parameter/PasswordChange.java
@@ -1,11 +1,12 @@
 package com.sample.parameter;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class PasswordChange {
 
-    private String oldPassword;
-    private String newPassword;
-    private String confirmationPassword;
+    private final String oldPassword;
+    private final String newPassword;
+    private final String confirmationPassword;
 }
