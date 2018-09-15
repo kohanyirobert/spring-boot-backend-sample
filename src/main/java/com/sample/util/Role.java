5777b8e2228940f32ff914a3ec16ca8609909503
Kohányi Róbert <kohanyi.robert@gmail.com>
2018-09-15 20:02:13 +0200

    Add enum that contains Spring roles

---
+++ src/main/java/com/sample/util/Role.java
@@ -0,0 +1,14 @@
+package com.sample.util;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+
+    ADMIN,
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
+}
